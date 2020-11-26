8a4b24b50c70c0bd58da0c4d61bb777a2f6bac47
Toni Sanmartin <tonisanmartin@MacBook-Pro-de-Toni.local>
2020-11-26 20:29:00 +0100

    feat erasing file

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+    const game = new Game('game-canvas');
+
+    document.addEventListener('keydown', (event) => {
+        game.onKeyEvent(event);
+      });
+      document.addEventListener('keyup', (event) => {
+        game.onKeyEvent(event);
+      })
+    
+      document.addEventListener('keypress', (event) => {
+        game.start();
+      })
+    });
